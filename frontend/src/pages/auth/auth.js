52d3d8a55cc85ea6a9eee64a24144760f92a4ffa
wenyuanhuizi <ywen37@emory.edu>
2025-04-11 11:50:23 -0400

    hardcoded login base url

--- frontend/src/pages/auth/auth.js
+++ frontend/src/pages/auth/auth.js
@@ -1,6 +1,8 @@
 // src/api/auth.js
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+// const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = "https://medease-454522.uc.r.appspot.com";
+
 
 export async function login(email, password) {
   const res = await fetch(`${BASE_URL}/auth/login`, {
